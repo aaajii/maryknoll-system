8333c256345aa1fe125d9e08b6d7f2c09e7fd7c2
Aji <jwpbanawan@addu.edu.ph>
2018-10-22 05:02:41 +0000

    Initialized system to work on c9.io

--- main/settings.py
+++ main/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+import django
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -151,3 +153,4 @@ MEDIA_URL = "/media/"
 
 
 
+django.setup()
